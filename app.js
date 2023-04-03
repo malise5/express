fe4b3d591603611adf4a60e296def4b57887bd31
Halkano Malise <kudezx@gmail.com>
2023-04-03 21:58:46 +0300

    fixed broken codes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 const app = express();
 const port = 6000;
+//middleware for posting data
+app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.status(200).json({ message: "Welcome from the SERVER sIDE" });
-// });
-
+//get the data from db.json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/db.json`));
 
+//GET
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -17,6 +18,20 @@ app.get("/api/v1/tours", (req, res) => {
       tours: tours,
     },
   });
+});
+
+//POST
+app.post("/api/v1/tours", (req, res) => {
+  console.log("================DATA================");
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id;
+  const newtour = Object.assign({ id: newId }, req.body);
+  tours.push(newtour);
+  fs.writeFile(`${__dirname}/db.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json{};
+  });
+  res.send("Done");
+  console.log("=================DATA================");
 });
 
 app.listen(port, () => {
