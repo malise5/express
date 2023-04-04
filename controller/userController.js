1bfc9559ebafe7822d6535c0e61aa4ed9d10cf0b
Halkano Malise <kudezx@gmail.com>
2023-04-04 12:42:56 +0300

    updated

---
+++ controller/userController.js
@@ -0,0 +1,36 @@
+/*========================================================== */
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "Not yet defined",
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "Not yet defined",
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "Not yet defined",
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "Not yet defined",
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "Not yet defined",
+  });
+};
+/*========================================================== */
