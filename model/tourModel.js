3c0cfbc364ab2e65f462f40916bd44f3ebbb3a90
Halkano Malise <kudezx@gmail.com>
2023-04-05 14:22:57 +0300

    updated modules

--- model/tourModel.js
+++ model/tourModel.js
@@ -6,6 +6,19 @@ const tourSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  durations: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group Size"],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
   rating: {
     type: Number,
     required: [true, "A  tour must have a Rating"],
