2fa2fad3c4973a60c268e8c787a9ea3ee313b2d0
Halkano Malise <kudezx@gmail.com>
2023-04-05 22:22:26 +0300

    added new routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,17 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  // checkID,
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require("../controller/tourController");
 
 const router = express.Router();
 
-// router.param("id", checkID);
+router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
+
+router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 /*========================================================== */
 
