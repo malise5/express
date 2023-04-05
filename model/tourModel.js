7c117da08689980470a60610da085883d0a3e18f
Halkano Malise <kudezx@gmail.com>
2023-04-05 15:03:15 +0300

    updated the model

--- model/tourModel.js
+++ model/tourModel.js
@@ -6,7 +6,7 @@ const tourSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  durations: {
+  duration: {
     type: Number,
     required: [true, "A tour must have a duration"],
   },
@@ -19,14 +19,38 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a difficulty"],
   },
-  rating: {
+  ratingsAverage: {
     type: Number,
     required: [true, "A  tour must have a Rating"],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, "A  tour must have a Price"],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a summary"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a Cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
